754c0203ab04f091afcd72d96fadbc7d7570d8d9
sdrocer <janosch_beer@gmx.de>
2025-01-27 19:43:38 +0100

    Add static path to image folder

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,7 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static', '../images']
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
