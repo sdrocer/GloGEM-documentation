5960f8425e47e210252e4ae012da6addaf5f6afe
sdrocer <janosch_beer@gmx.de>
2025-01-28 16:04:43 +0100

    Update js file to adjust custom theme

--- docs/source/_static/custom.js
+++ docs/source/_static/custom.js
@@ -3,7 +3,15 @@ document.addEventListener("DOMContentLoaded", function() {
     var sidebar = document.querySelector('.wy-nav-side');
     if (sidebar) {
         sidebar.addEventListener('click', function(event) {
-            event.preventDefault();
+            // Check if the clicked element is a link
+            if (event.target.tagName === 'A') {
+                // Prevent the default behavior only if the link is within the sidebar
+                if (event.target.closest('.wy-nav-side')) {
+                    event.preventDefault();
+                    // Manually handle the navigation
+                    window.location.href = event.target.href;
+                }
+            }
         });
     }
 });
