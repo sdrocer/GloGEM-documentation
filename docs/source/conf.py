dea4768e5827e7ffa7d2e61688dadb7c5d3c84ea
sdrocer <janosch_beer@gmx.de>
2025-01-28 16:22:56 +0100

    Adjust html_theme_options

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
 
 html_theme_options = {
-    "repository_url": "https://github.com/your-repo",
+    "repository_url": "https://github.com/sdrocer/GloGEM-documentation.git",
     "use_repository_button": True,
 }
 
