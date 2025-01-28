c07f05a489f522429996a54bfd2da74bf02035fa
sdrocer <janosch_beer@gmx.de>
2025-01-28 16:10:24 +0100

    Make sidebar entirely static, only mouse scrolling can move it.

--- docs/source/_static/custom.js
+++ docs/source/_static/custom.js
@@ -8,8 +8,16 @@ document.addEventListener("DOMContentLoaded", function() {
                 // Prevent the default behavior only if the link is within the sidebar
                 if (event.target.closest('.wy-nav-side')) {
                     event.preventDefault();
-                    // Manually handle the navigation
-                    window.location.href = event.target.href;
+                    // Manually handle the navigation without causing the sidebar to move
+                    var targetId = event.target.getAttribute('href').split('#')[1];
+                    if (targetId) {
+                        var targetElement = document.getElementById(targetId);
+                        if (targetElement) {
+                            targetElement.scrollIntoView({ behavior: 'smooth' });
+                        }
+                    } else {
+                        window.location.href = event.target.href;
+                    }
                 }
             }
         });
